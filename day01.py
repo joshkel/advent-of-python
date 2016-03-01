9d6bc9b3f738aa04369860c52f27cf856620dcf9
Josh Kelley <joshkel@gmail.com>
2016-03-01 09:30:54 -0500

    Day 1 part 1 working

--- day01.py
+++ day01.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
-if __name__ == '__main__':
-    pass
+import fileinput
 
+def elevator(s):
+    return s.count('(') - s.count(')')
+
+if __name__ == '__main__':
+    s = ''
+    for line in fileinput.input():
+        s += line
+    print("Final floor: %i" % elevator(s))
--- tests.py
+++ tests.py
@@ -2,5 +2,16 @@
 
 import unittest
 
+from day01 import elevator
+
+class TestElevator(unittest.TestCase):
+    def test_simple(self):
+        self.assertEqual(elevator('(())'), 0)
+        self.assertEqual(elevator('()()'), 0)
+
+    def test_3(self):
+        self.assertEqual(elevator('((('), 3)
+        self.assertEqual(elevator('(()(()('), 3)
+
 if __name__ == '__main__':
     unittest.main()
