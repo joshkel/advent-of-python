a571e6c842d0ca9e67e8dab500e1c0c6d06f16ec
Josh Kelley <joshkel@gmail.com>
2016-03-15 22:23:59 -0400

    Experiment with multithreading

--- day04.py
+++ day04.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 from hashlib import md5
+from concurrent.futures import ProcessPoolExecutor
+import multiprocessing
 import sys
 
-def advent_coin(key, count):
-    i = 1
+def advent_coin(key, count, start=1, stride=1):
+    i = start
     key = key.encode('ascii')
     search = '0' * count
     m = md5()
@@ -15,7 +17,13 @@ def advent_coin(key, count):
         digest = m2.hexdigest()
         if digest[:count] == search:
             return i
-        i += 1
+        i += stride
+
+def advent_coin_multithreaded(key, count):
+    cpus = multiprocessing.cpu_count()
+    with ProcessPoolExecutor(max_workers=cpus) as executor:
+        futures = [executor.submit(advent_coin, key, count, i + 1, cpus) for i in range(cpus)]
+        return min([f.result() for f in futures])
 
 if __name__ == '__main__':
     _, key, count = sys.argv
