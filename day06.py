1541f78eb01589ad255be73e8af83d819dffe637
Josh Kelley <joshkel@gmail.com>
2016-03-22 22:33:53 -0400

    Day 6 part 2

--- day06.py
+++ day06.py
@@ -15,37 +15,55 @@ class Range(List):
         x2, y2 = self[1]
         return lights[x1:x2+1, y1:y2+1]
 
+    def apply(self, lights, f):
+        l = self.slice(lights)
+        l[:] = f(l)
+
 class ToggleCommand:
     grammar = 'toggle', attr('range', Range)
 
-    def action(self, lights):
-        l = self.range.slice(lights)
-        l[:] = 1 - l
+    def simple_action(self, lights):
+        # For a bool, 1 - b is equivalent to not b
+        self.range.apply(lights, lambda l: 1 - l)
+
+    def fancy_action(self, lights):
+        self.range.apply(lights, lambda l: l + 2)
 
 class TurnOffCommand:
     grammar = 'turn off', attr('range', Range)
 
-    def action(self, lights):
-        l = self.range.slice(lights)
-        l[:] = False
+    def simple_action(self, lights):
+        self.range.apply(lights, lambda l: False)
+
+    def fancy_action(self, lights):
+        self.range.apply(lights, lambda l: np.fmax(l - 1, 0))
 
 class TurnOnCommand:
     grammar = 'turn on', attr('range', Range)
 
-    def action(self, lights):
-        l = self.range.slice(lights)
-        l[:] = True
+    def simple_action(self, lights):
+        self.range.apply(lights, lambda l: True)
+
+    def fancy_action(self, lights):
+        self.range.apply(lights, lambda l: l + 1)
 
 commands = [ToggleCommand, TurnOnCommand, TurnOffCommand]
 
-def make_lights():
-    return np.zeros((1000,1000), dtype=np.bool)
+def make_lights(dtype):
+    return np.zeros((1000,1000), dtype=dtype)
 
 if __name__ == '__main__':
-    lights = make_lights()
+    simple_lights = make_lights(np.bool)
+    fancy_lights = make_lights(np.int32)
+
     for line in fileinput.input():
         cmd = parse(line, commands)
-        cmd.action(lights)
-    print(lights)
-    print("Total lights: %i" % np.sum(lights))
+        cmd.simple_action(simple_lights)
+        cmd.fancy_action(fancy_lights)
+
+    print(simple_lights)
+    print("Total lights (simple): %i" % np.sum(simple_lights))
+
+    print(fancy_lights)
+    print("Total lights (fancy): %i" % np.sum(fancy_lights))
 
