9400a36531dd145129b2f10ade62b9a563e79e8f
Josh Kelley <joshkel@gmail.com>
2016-03-22 22:04:49 -0400

    Minimally functional parsing and execution

--- day06.py
+++ day06.py
@@ -1,34 +1,44 @@
 #!/usr/bin/env python3
 
 from pypeg2 import *
-
-class ToggleCommand(str):
-    grammar = 'toggle'
-
-    def action(self):
-        print('Switcheroo!')
-
-class TurnOffCommand(str):
-    grammar = 'turn off'
-
-    def action(self):
-        print('Power off')
-
-class TurnOnCommand(str):
-    grammar = 'turn on'
-
-    def action(self):
-        print('Turn it on!')
-
-Command = [ToggleCommand, TurnOnCommand, TurnOffCommand]
+import numpy as np
 
 class Coord(List):
     grammar = int, ',', int
 
-class Command(List):
-    grammar = Command, Coord, 'through', Coord
+class Range(List):
+    grammar = Coord, 'through', Coord
 
-result = parse('turn off 1,2 through 5,6', Command)
-print(result)
-result[0].action()
-print(result[1][1])
+    def apply(self, lights, f):
+        for i in range(self[0][0], self[1][0] + 1):
+            for j in range(self[0][1], self[1][1] + 1):
+                if callable(f):
+                    lights[i][j] = f(lights[i][j])
+                else:
+                    lights[i][j] = f
+
+class ToggleCommand:
+    grammar = 'toggle', attr('range', Range)
+
+    def action(self, lights):
+        self.range.apply(lights, lambda l: not l)
+
+class TurnOffCommand:
+    grammar = 'turn off', attr('range', Range)
+
+    def action(self, lights):
+        self.range.apply(lights, False)
+
+class TurnOnCommand:
+    grammar = 'turn on', attr('range', Range)
+
+    def action(self, lights):
+        self.range.apply(lights, True)
+
+commands = [ToggleCommand, TurnOnCommand, TurnOffCommand]
+
+lights = np.zeros((1000,1000), dtype=np.bool)
+cmd = parse('toggle 0,0 through 999,999', commands)
+cmd.action(lights)
+print(lights)
+print("Total lights: %i" % np.sum(lights))
